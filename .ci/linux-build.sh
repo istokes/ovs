4237712c3b0a0040f6c9fbd2f02157ad06321700
Sunil Pai G <sunil.pai.g@intel.com>
2022-09-27 13:22:53 +0000

    ci: Enable builds to test with dependent DSA branches.
    Signed-off-by: Sunil Pai G <sunil.pai.g@intel.com>
    Signed-off-by: Ian Stokes <ian.stokes@intel.com>

--- .ci/linux-build.sh
+++ .ci/linux-build.sh
@@ -13,7 +13,6 @@ EXTRA_OPTS="--enable-Werror"
 function install_dpdk()
 {
     local DPDK_VER=$1
-    local VERSION_FILE="dpdk-dir/cached-version"
     local DPDK_OPTS=""
     local DPDK_LIB=$(pwd)/dpdk-dir/build/lib/x86_64-linux-gnu
 
@@ -27,32 +26,13 @@ function install_dpdk()
     # Export the following path for pkg-config to find the .pc file.
     export PKG_CONFIG_PATH=$DPDK_LIB/pkgconfig/:$PKG_CONFIG_PATH
 
-    if [ "${DPDK_VER##refs/*/}" != "${DPDK_VER}" ]; then
-        # Avoid using cache for git tree build.
-        rm -rf dpdk-dir
+    # Avoid using cache for git tree build.
+    rm -rf dpdk-dir
 
-        DPDK_GIT=${DPDK_GIT:-https://dpdk.org/git/dpdk}
-        git clone --single-branch $DPDK_GIT dpdk-dir -b "${DPDK_VER##refs/*/}"
-        pushd dpdk-dir
-        git log -1 --oneline
-    else
-        if [ -f "${VERSION_FILE}" ]; then
-            VER=$(cat ${VERSION_FILE})
-            if [ "${VER}" = "${DPDK_VER}" ]; then
-                # Update the library paths.
-                sudo ldconfig
-                echo "Found cached DPDK ${VER} build in $(pwd)/dpdk-dir"
-                return
-            fi
-        fi
-        # No cache or version mismatch.
-        rm -rf dpdk-dir
-        wget https://fast.dpdk.org/rel/dpdk-$1.tar.xz
-        tar xvf dpdk-$1.tar.xz > /dev/null
-        DIR_NAME=$(tar -tf dpdk-$1.tar.xz | head -1 | cut -f1 -d"/")
-        mv ${DIR_NAME} dpdk-dir
-        pushd dpdk-dir
-    fi
+    DPDK_GIT="https://github.com/istokes/dpdk.git"
+    git clone --single-branch $DPDK_GIT dpdk-dir -b "${DPDK_VER}"
+    pushd dpdk-dir
+    git log -1 --oneline
 
     # Switching to 'default' machine to make dpdk-dir cache usable on
     # different CPUs. We can't be sure that all CI machines are exactly same.
@@ -74,16 +54,13 @@ function install_dpdk()
     DPDK_OPTS="$DPDK_OPTS --prefix=$(pwd)/build"
 
     CC=gcc meson $DPDK_OPTS build
-    ninja -C build
     ninja -C build install
 
     # Update the library paths.
     sudo ldconfig
 
-
     echo "Installed DPDK source in $(pwd)"
     popd
-    echo "${DPDK_VER}" > ${VERSION_FILE}
 }
 
 function configure_ovs()
@@ -133,13 +110,9 @@ assert ovs.json.from_string('{\"a\": 42}') == {'a': 42}"
 fi
 
 if [ "$DPDK" ]; then
-    if [ -z "$DPDK_VER" ]; then
-        DPDK_VER="22.11.1"
-    fi
+    DPDK_VER="dma-tracking"
     install_dpdk $DPDK_VER
-    if [ -n "$DPDK_EXPERIMENTAL" ]; then
-        CFLAGS_FOR_OVS="${CFLAGS_FOR_OVS} -DALLOW_EXPERIMENTAL_API"
-    fi
+    CFLAGS_FOR_OVS="${CFLAGS_FOR_OVS} -DALLOW_EXPERIMENTAL_API"
 fi
 
 if [ "$CC" = "clang" ]; then
